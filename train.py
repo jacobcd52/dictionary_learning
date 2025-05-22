f9c3964923f481cacd349849782bc037d71fef6b
jacobcd52 <jcdguitarguy@googlemail.com>
2025-05-22 16:38:26 +0000

    started sweep

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ N_CPUS = 19 // 2
 
 
 PATH_TO_PILE = "/root/dictionary_learning/pile-uncopyrighted"
-N_TOKENS = 10_000
+N_TOKENS = 100_000_000
 CFG = SCAEConfig(
     model_name="EleutherAI/pythia-70m",
     wb_project="pythia_scae_cc",
@@ -25,12 +25,12 @@ CFG = SCAEConfig(
     warmup_ratio=0.00,
     decay_start_ratio=0.7,
     epochs=1,
-    batch_size=32,
+    batch_size=128,
     k=64,
     expansion_factor=8,
     sample_length=128,
     track_dead_features=True,
-    base_lr=1e-3,
+    base_lr=5e-4,
     target_C=0,
     # auxk_alpha=0.0,
     mask_type="simple",
@@ -38,7 +38,7 @@ CFG = SCAEConfig(
     ce_loss_sparse_coeff=0,
     fvu_loss_coeff = 1.0,
     fvu_loss_sparse_coeff=1.0,
-    feature_act_fvu_coeff=0.02,
+    feature_act_fvu_coeff=0.1,
     mask_loss_coeff=2e-5,
 )
 
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     tokenizer.pad_token = tokenizer.eos_token
     dataset = load_dataset(
         PATH_TO_PILE,
-        split="train[:10%]",
+        split="train[:20%]",
         num_proc=N_CPUS,
     )
 
@@ -60,11 +60,15 @@ if __name__ == "__main__":
     world_size = t.cuda.device_count()
     print(f"Using {world_size} GPUs")
 
-    CFG.wb_run_name = f"mask{CFG.mask_loss_coeff} fact_fvu{CFG.feature_act_fvu_coeff} fvu_sparse{CFG.fvu_loss_sparse_coeff} fvu{CFG.fvu_loss_coeff} lr{CFG.base_lr}"
+    mask_loss_coeffs_sweep = [1e-4, 3e-5, 1e-5, 0]
 
-    mp.spawn(
-        SCAETrainer,
-        args=(world_size, t.bfloat16, CFG, dataset),
-        nprocs=world_size,
-        join=True,
-    )
+    for mask_loss_val in mask_loss_coeffs_sweep:
+        CFG.mask_loss_coeff = mask_loss_val
+        CFG.wb_run_name = f"mask{CFG.mask_loss_coeff} fact_fvu{CFG.feature_act_fvu_coeff} fvu_sparse{CFG.fvu_loss_sparse_coeff} fvu{CFG.fvu_loss_coeff} lr{CFG.base_lr}"
+
+        mp.spawn(
+            SCAETrainer,
+            args=(world_size, t.bfloat16, CFG, dataset),
+            nprocs=world_size,
+            join=True,
+        )
